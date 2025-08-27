970f0b28251aae4943b033efbd638c1b7daf14f7
Ronald Chaves <ronaldchavesgonzalez@gmail.com>
2025-08-26 23:28:46 -0600

    feat(package.json, App.tsx, App.css, vite.config): Add tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import { Outlet, NavLink } from "react-router";
 function App() {
   return (
     <>
-      <main>
-        <nav>
+      <main className="box-border">
+        <nav className="flex flex-row gap-6">
           <NavLink to={"/users"}>List Users</NavLink>
           <NavLink to={"/users/create"}>Create User</NavLink>
           <NavLink to={"/users/:id"}>Update User</NavLink>
